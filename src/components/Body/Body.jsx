3cf170f07367b10d8ba3e5aaf001a26637681640
composercordero <composercordero@gmail.com>
2023-11-14 22:40:44 -0600

    body layout

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,8 +1,20 @@
-import classes from './Body.module.css'
+import example from '../../assets/hero_austin.png'
+
 
 const Body = () => {
     return(<>
-    
+
+    <div className="grid grid-cols-12 grid-rows-1 gap-9">
+        <div className='col-span-3'>Filter</div>
+        <div className='col-span-9'>
+            <h1>Events</h1>
+            <img src={example} className=''/>
+            <h3>Title</h3>
+            <p>Description</p>
+        </div>
+    </div>
+
+
     </>)
 };
 
